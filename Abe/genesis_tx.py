318a78f4bf20d7fe0a45eacdfbd35fc2ab4739e9
Evan Duffield <evan@darkcoin.io>
2014-04-22 08:25:24 -0700

    first commit

---
+++ Abe/genesis_tx.py
@@ -0,0 +1,38 @@
+# Copyright(C) 2013 by John Tobey <jtobey@john-edwin-tobey.org>
+
+# genesis_tx.py: known transactions unavailable through RPC for
+# historical reasons: https://bitcointalk.org/index.php?topic=119530.0
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/agpl.html>.
+
+def get(tx_hash_hex):
+    """
+    Given the hexadecimal hash of the genesis transaction (as shown
+    by, e.g., "bitcoind getblock 0") return the hexadecimal raw
+    transaction.  This works around a Bitcoind limitation described at
+    https://bitcointalk.org/index.php?topic=119530.0
+    """
+
+    # Main Bitcoin chain:
+    if tx_hash_hex == "868b2fb28cb1a0b881480cc85eb207e29e6ae75cdd6d26688ed34c2d2d23c776":
+        return "01000000010000000000000000000000000000000000000000000000000000000000000000FFFFFFFF4A04FFFF001D0104423231204A756C7920323031332C2054686520477561726469616E2C20546573636F20626F7373207361797320636865617020666F6F6420657261206973206F766572FFFFFFFF01A086010000000000434104678AFDB0FE5548271967F1A67130B7105CD6A828E03909A67962E0EA1F61DEB649F6BC3F4CEF38C4F35504E51EC112DE5C384DF7BA0B8D578A4C702B6BF11D5FAC00000000"
+
+    # Extract your chain's genesis transaction data from the first
+    # block file and add it here, or better yet, patch your coin's
+    # getrawtransaction to return it on request:
+    #if tx_hash_hex == "<HASH>"
+    #    return "<DATA>"
+
+    return None
